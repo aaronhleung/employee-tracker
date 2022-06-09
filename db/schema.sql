9c19dba1d4af84acd202ffcf942f80eec23dc181
aaronhleung <aaronleung@outlook.com>
2022-06-09 08:52:12 -0700

    created db schema

---
+++ db/schema.sql
@@ -0,0 +1,26 @@
+DROP DATABASE IF EXISTS departments;
+DROP DATABASE IF EXISTS roles;
+DROP DATABASE IF EXISTS employees;
+
+
+CREATE TABLE departments (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE roles (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INTEGER NOT NULL,
+    FOREIGN KEY (department_id) REFERENCES departments(id),
+);
+
+CREATE TABLE employees (
+    employee_id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER NOT NULL,
+    manager_id INTEGER,
+    FOREIGN KEY (role_id) REFERENCES role(id)
+);
